1c9a9995824d09b0122c3db2f3f1521ecf615c8a
wuxx <wujianhua0924@gmail.com>
2016-02-19 22:51:17 +0000

    building the framework

--- simulator.c
+++ simulator.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-typedef  unsigned int u32;
-typedef  signed   int s32;
+#define MEM_SIZE (10*1024)
+#define error() do {printf("error: [%s][%d]\n", __func__, __LINE__); exit(-1);} while(0)
+
+typedef  unsigned char u8;
+typedef  signed   char s8;
+typedef  unsigned int  u32;
+typedef  signed   int  s32;
 
 typedef void (*op_handler)(u32 op);
 
@@ -16,31 +26,31 @@ enum OP_TYPE_E {
 
 enum SUB_TYPE_E {
 
-    MOV = OP_ADDRESSING << 16 |  0,
-    LDR = OP_ADDRESSING << 16 |  1,
-    STR = OP_ADDRESSING << 16 |  2,
+    MOV = OP_ADDRESSING << 8 |  0,
+    LDR = OP_ADDRESSING << 8 |  1,
+    STR = OP_ADDRESSING << 8 |  2,
 
-    PUSH = OP_STACK << 16 | 3,
-    POP  = OP_STACK << 16 | 4,
+    PUSH = OP_STACK << 8 | 3,
+    POP  = OP_STACK << 8 | 4,
 
-    CALL = OP_FUNC_CALL << 16 |  5,
-    RET  = OP_FUNC_CALL << 16 |  6,
+    CALL = OP_FUNC_CALL << 8 |  5,
+    RET  = OP_FUNC_CALL << 8 |  6,
 
-    ADD = OP_ARITHMETIC << 16 |  7,
-    SUB = OP_ARITHMETIC << 16 |  9,
-    DIV = OP_ARITHMETIC << 16 |  8,
-    MUL = OP_ARITHMETIC << 16 | 10,
-    AND = OP_ARITHMETIC << 16 | 11,
-    OR  = OP_ARITHMETIC << 16 | 12,
-    XOR = OP_ARITHMETIC << 16 | 13,
+    ADD = OP_ARITHMETIC << 8 |  7,
+    SUB = OP_ARITHMETIC << 8 |  9,
+    DIV = OP_ARITHMETIC << 8 |  8,
+    MUL = OP_ARITHMETIC << 8 | 10,
+    AND = OP_ARITHMETIC << 8 | 11,
+    OR  = OP_ARITHMETIC << 8 | 12,
+    XOR = OP_ARITHMETIC << 8 | 13,
 
-    JMP   = OP_JMP << 16 | 14,
-    JMPN  = OP_JMP << 16 | 15,
-    JMPZ  = OP_JMP << 16 | 16,
-    JMPO  = OP_JMP << 16 | 17,
-    JMPNN = OP_JMP << 16 | 18,
-    JMPNZ = OP_JMP << 16 | 19,
-    JMPNO = OP_JMP << 16 | 20,
+    JMP   = OP_JMP << 8 | 14,
+    JMPN  = OP_JMP << 8 | 15,
+    JMPZ  = OP_JMP << 8 | 16,
+    JMPO  = OP_JMP << 8 | 17,
+    JMPNN = OP_JMP << 8 | 18,
+    JMPNZ = OP_JMP << 8 | 19,
+    JMPNO = OP_JMP << 8 | 20,
     
 };
 
@@ -51,8 +61,9 @@ enum ADDRESS_MODE_E {
 };
 
 struct __instruction__ {
-    u32 sub_type: 16;
-    u32 op_type:   3;
+    u32 op_type:  16;
+    u32 reserved:  4;
+    u32 src2:      2;
     u32 am_src2:   2;
     u32 src1:      2;
     u32 am_src1:   2;
@@ -82,6 +93,9 @@ struct __cpu__ cpu =
     .pc   = 0,
     .flag = 0,
 };
+
+u8 cpu_mem[MEM_SIZE] = {0};
+int ifd = 0, cpu_cycles = 0;
 
 void op_mov(u32 op)
 {
@@ -168,45 +182,93 @@ void op_jmpno(u32 op)
 }
 
 struct __instruction_set__ is[] = {
-    {"", MOV, op_mov},
-    {"", LDR, op_ldr},
-    {"", STR, op_str},
+    {"mov",   MOV,   op_mov},
+    {"ldr",   LDR,   op_ldr},
+    {"str",   STR,   op_str},
 
-    {"", PUSH, op_push},
-    {"", POP,  op_pop},
+    {"push",  PUSH,  op_push},
+    {"pop",   POP,   op_pop},
 
-    {"", CALL, op_call},
-    {"", RET,  op_ret},
+    {"call",  CALL,  op_call},
+    {"ret",   RET,   op_ret},
 
-    {"", ADD, op_add},
-    {"", SUB, op_sub},
-    {"", MUL, op_mul},
-    {"", DIV, op_div},
-    {"", AND, op_and},
-    {"", OR,  op_or},
-    {"", XOR, op_xor},
+    {"add",   ADD,   op_add},
+    {"sub",   SUB,   op_sub},
+    {"mul",   MUL,   op_mul},
+    {"div",   DIV,   op_div},
+    {"and",   AND,   op_and},
+    {"or",    OR,    op_or},
+    {"xor",   XOR,   op_xor},
 
-    {"", JMP,   op_jmp},
-    {"", JMPN,  op_jmpn},
-    {"", JMPZ,  op_jmpz},
-    {"", JMPO,  op_jmpo},
-    {"", JMPNN, op_jmpnn},
-    {"", JMPNZ, op_jmpnz},
-    {"", JMPNO, op_jmpno},
+    {"jmp",   JMP,   op_jmp},
+    {"jmpn",  JMPN,  op_jmpn},
+    {"jmpz",  JMPZ,  op_jmpz},
+    {"jmpo",  JMPO,  op_jmpo},
+    {"jmpnn", JMPNN, op_jmpnn},
+    {"jmpnz", JMPNZ, op_jmpnz},
+    {"jmpno", JMPNO, op_jmpno},
 };
 
 s32 is_legal(u32 op)
 {
+    u32 i;
+    for(i=0;i<sizeof(is)/sizeof(is[0]);i++) {
+        if ((op & 0xFFFF) == is[i].operation) {
+            return 1;
+        }
+    }
     return 0;
 }
 
-u32 get_word()
+u32 cpu_read_mem(u32 addr)
 {
-    return 0;
+    u32 *word;
+    assert((addr % 4) == 0);
+    assert(addr < MEM_SIZE);
+    word = (u32 *)(&cpu_mem[addr]);
+
+
+    return *word;
+}
+
+/* run 1 cycle, we always treat that 1 instruction consume 1 cycle */
+void cpu_run()
+{
+    u32 count;
+    s32 op;
 }
 
 /* simutor my cpu */
-int main()
+int main(int argc, char **argv)
 {
+    struct stat st;
+
+    if (argc != 2) {
+        printf("%s [code.bin]\n", argv[0]);
+        exit(-1);
+    }
+
+    if ((ifd = open(argv[1], O_RDONLY)) == -1) {
+        perror("open");
+        exit(-1);
+    }
+
+    if (fstat(ifd, &st) == -1) {
+        perror("fstat");
+        exit(-1);
+    }
+
+    printf("file size: %d \n", st.st_size);
+    if ((st.st_size % 4) != 0) {
+        printf("%s size not multiple by 4 \n");
+        exit(-1);
+    }
+
+    assert(st.st_size < MEM_SIZE);
+
+    while (1) {
+        cpu_run();
+        cpu_cycles++;
+    }
     return 0;
 }
