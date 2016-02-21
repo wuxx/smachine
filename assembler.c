39ce96fad5ae436305f55b50df75b62b224be967
wuxx <wujianhua0924@gmail.com>
2016-02-21 07:36:19 +0000

    add cpu.h

--- assembler.c
+++ assembler.c
@@ -11,14 +11,15 @@
 */
 
 #define POOL_SIZE 1024
-typedef  unsigned char u8;
-typedef  signed   char s8;
-typedef  unsigned int  u32;
-typedef  signed   int  s32;
+
+typedef unsigned char u8;
+typedef signed   char s8;
+typedef unsigned int  u32;
+typedef signed   int  s32;
 
 enum TOKEN_TYPE_E {
     TOKEN_KEYWORD = 0,
-    TOKEN_GOTO    = 1,  /* [goto 0x1234] */
+    TOKEN_LOCATE  = 1,  /* [locate 0x1234] */
     TOKEN_COMMA   = 2,  
     TOKEN_STRING  = 3,  /* str: "hello, world" */
     TOKEN_COLON   = 4,  /* a: jmp a */
@@ -41,12 +42,32 @@ char *keyword[] = { "mov",  "ldr",  "str",
 
 struct __token__ token_pool[POOL_SIZE];
 
-int parse_token()
+int parse_token(char *ifile)
 {
+    FILE *fp;
+    char line[1024];
+    if((fp = fopen(ifile, "r")) == NULL)
+    { 
+        perror("fopen"); 
+        exit(-1);
+    } 
+
+    while (!feof(fp)) 
+    { 
+        fgets(line,sizeof(line),fp);
+        printf("%s\n", line);
+    } 
 
 }
 
 int main(int argc, char **argv)
 {
+
+    if (argc != 2) {
+        printf("%s [foo.s]\n", argv[0]);
+        exit(-1);
+    }
+
+    parse_token(argv[1]);
     return 0;
 }
