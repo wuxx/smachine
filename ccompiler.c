1a4661ce6be1147ceda16b31bf863a1c0010496a
wuxx <wujianhua0924@gmail.com>
2016-03-13 11:43:44 +0000

    parse_token coding.

--- ccompiler.c
+++ ccompiler.c
@@ -11,48 +11,50 @@
 #define POOL_SIZE (10240)
 
 enum TOKEN_TYPE_E {
-    TOKEN_KW_IF = 0,        /* "if"     */
-    TOKEN_KW_ELSE,          /* "else"   */
-    TOKEN_KW_WHILE,         /* "while"  */
-    TOKEN_KW_FOR,           /* "for"    */
-    TOKEN_KW_DO,            /* "do"     */
-    TOKEN_KW_RETURN,        /* "return" */
+    TOKEN_KW_IF = 0,         
+    TOKEN_KW_ELSE,       
+    TOKEN_KW_WHILE,      
+    TOKEN_KW_FOR,        
+    TOKEN_KW_DO,         
+    TOKEN_KW_RETURN,     
 
-    TOKEN_TP_VOID,          /* "void" */
-    TOKEN_TP_INT,           /* "int"  */
-    TOKEN_TP_CHAR,          /* "char" */
-    TOKEN_TP_ENUM,          /* "enum" */
+    TOKEN_TP_VOID,       
+    TOKEN_TP_INT,        
+    TOKEN_TP_CHAR,       
+    TOKEN_TP_ENUM,       
 
-    TOKEN_OP_PLUS,          /* "+" */
-    TOKEN_OP_SUB,           /* "-" */
-    TOKEN_OP_MUL,           /* "*" */
-    TOKEN_OP_DIV,           /* "/" */
-    TOKEN_OP_EQ,            /* "=" */
-    TOKEN_OP_EM,            /* "!" exclamation mark */
-    TOKEN_OP_TILDE,         /* "~"  */
-    TOKEN_OP_XOR,           /* "^"  */
-    TOKEN_OP_PERCENT,       /* "%"  */
-    TOKEN_OP_AND,           /* "&&" */
-    TOKEN_OP_OR,            /* "||" */
-    TOKEN_OP_PP,            /* "++" */
-    TOKEN_OP_SS,            /* "--" */
-    TOKEN_OP_EE,            /* "==" */
-    TOKEN_OP_LT,            /* "<"  */
-    TOKEN_OP_GT,            /* ">"  */
-    TOKEN_OP_LE,            /* "<=" */
-    TOKEN_OP_GE,            /* ">=" */
+    TOKEN_OP_PLUS,       
+    TOKEN_OP_SUB,        
+    TOKEN_OP_MUL,        
+    TOKEN_OP_DIV,        
+    TOKEN_OP_EQ,         
+    TOKEN_OP_EM,         
+    TOKEN_OP_TILDE,      
+    TOKEN_OP_XOR,        
+    TOKEN_OP_PERCENT,    
+    TOKEN_OP_ADDR,       
+    TOKEN_OP_LAND,       
+    TOKEN_OP_OR,         
+    TOKEN_OP_LOR,        
+    TOKEN_OP_PP,         
+    TOKEN_OP_SS,         
+    TOKEN_OP_EE,         
+    TOKEN_OP_LT,         
+    TOKEN_OP_GT,         
+    TOKEN_OP_LE,         
+    TOKEN_OP_GE,         
 
-    TOKEN_SP_LCURBRACE,     /* "{"  */
-    TOKEN_SP_RCURBRACE,     /* "}"  */
-    TOKEN_SP_LSQRBRACKET,   /* "["  */
-    TOKEN_SP_RSQRBRACKET,   /* "]"  */
-    TOKEN_SP_LRNDBRACKET,   /* "("  */
-    TOKEN_SP_RRNDBRACKET,   /* "("  */
-    TOKEN_SP_SEMICOLON,     /* ";"  */
-    TOKEN_SP_COMMA,         /* ","  */
+    TOKEN_SP_LCURBRACE,  
+    TOKEN_SP_RCURBRACE,  
+    TOKEN_SP_LSQRBRACKET,
+    TOKEN_SP_RSQRBRACKET,
+    TOKEN_SP_LRNDBRACKET,
+    TOKEN_SP_RRNDBRACKET,
+    TOKEN_SP_SEMICOLON,  
+    TOKEN_SP_COMMA,      
 
-    TOKEN_ID,               /* user-defined */
-
+    TOKEN_ID,            
+    TOKEN_NUM,           
     TOKEN_MAX,
 }; 
 
@@ -89,8 +91,10 @@ struct __token__ c_token_pool[] = {
     {TOKEN_OP_TILDE,         "~",        0},
     {TOKEN_OP_XOR,           "^",        0},
     {TOKEN_OP_PERCENT,       "%",        0},
-    {TOKEN_OP_AND,           "&&",       0},
-    {TOKEN_OP_OR,            "||",       0},
+    {TOKEN_OP_ADDR,          "&",        0},
+    {TOKEN_OP_LAND,          "&&",       0},
+    {TOKEN_OP_OR,            "|",        0},
+    {TOKEN_OP_LOR,           "||",       0},
     {TOKEN_OP_PP,            "++",       0},
     {TOKEN_OP_SS,            "--",       0},
     {TOKEN_OP_EE,            "==",       0},
@@ -129,6 +133,16 @@ int is_operator(char c)
 
 int is_separator(char c)
 {
+
+}
+
+int is_digit(char c)
+{
+    if (c >= '0' && c <= '9') {
+        return 1;
+    } else {
+        return 0;
+    }
 
 }
 
@@ -184,61 +198,89 @@ void unget_char()
 
 int parse_token()
 {
-    char c;
-
-    {TOKEN_OP_PLUS,          "+",        0},
-    {TOKEN_OP_SUB,           "-",        0},
-    {TOKEN_OP_MUL,           "*",        0},
-    {TOKEN_OP_DIV,           "/",        0},
-    {TOKEN_OP_EQ,            "=",        0},
-    {TOKEN_OP_EM,            "!",        0},
-    {TOKEN_OP_TILDE,         "~",        0},
-    {TOKEN_OP_OR,            "|",        0},
-    {TOKEN_OP_XOR,           "^",        0},
-    {TOKEN_OP_PERCENT,       "%",        0},
-    {TOKEN_OP_ADDR,          "&",        0},
-    {TOKEN_OP_LAND,          "&&",       0},
-    {TOKEN_OP_LOR,           "||",       0},
-    {TOKEN_OP_PP,            "++",       0},
-    {TOKEN_OP_SS,            "--",       0},
-    {TOKEN_OP_EE,            "==",       0},
-    {TOKEN_OP_LT,            "<",        0},
-    {TOKEN_OP_GT,            ">",        0},
-    {TOKEN_OP_LE,            "<=",       0},
-    {TOKEN_OP_GE,            ">=",       0},
-
-    {TOKEN_SP_LCURBRACE,     "{",        0},
-    {TOKEN_SP_RCURBRACE,     "}",        0},
-    {TOKEN_SP_LSQRBRACKET,   "[",        0},
-    {TOKEN_SP_RSQRBRACKET,   "]",        0},
-    {TOKEN_SP_LRNDBRACKET,   "(",        0},
-    {TOKEN_SP_RRNDBRACKET,   ")",        0},
-    {TOKEN_SP_SEMICOLON,     ";",        0},
-    {TOKEN_SP_COMMA,         ",",        0},
+    char c, cn;
     c = get_char();
     while (c != -1) {
         if (is_id_head(c)) {        /* keyword id, type id, or user-defined id */
 
+        } else if (is_digit(c)) {
         } else if (c == '+') {      /* "+" or "++" */
+            if ((cn = get_char()) == '+') {
+                put_token(TOKEN_OP_PP, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_PLUS, 0);
+            }
         } else if (c == '-') {      /* "-" or "--" */
+            if ((cn = get_char()) == '-') {
+                put_token(TOKEN_OP_SS, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_SUB, 0);
+            }
         } else if (c == '*') {
+            put_token(TOKEN_OP_MUL, 0);
         } else if (c == '/') {
+            put_token(TOKEN_OP_DIV, 0);
         } else if (c == '=') {      /* "=" or "==" */
+            if ((cn = get_char()) == '=') {
+                put_token(TOKEN_OP_EE, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_EQ, 0);
+            }
         } else if (c == '~') {
+            put_token(TOKEN_OP_TILDE, 0);
         } else if (c == '|') {      /* "|" or "||" */
+            if ((cn = get_char()) == '|') {
+                put_token(TOKEN_OP_LOR, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_OR, 0);
+            }
         } else if (c == '^') {
+            put_token(TOKEN_OP_XOR, 0);
         } else if (c == '%') {
+            put_token(TOKEN_OP_PERCENT, 0);
         } else if (c == '&') {      /* "&" or "&&" */
+            if ((cn = get_char()) == '&') {
+                put_token(TOKEN_OP_LAND, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_ADDR, 0);
+            }
         } else if (c == '<') {      /* "<" or "<=" */
+            if ((cn = get_char()) == '=') {
+                put_token(TOKEN_OP_LE, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_LT, 0);
+            }
         } else if (c == '>') {      /* ">" or ">=" */
+            if ((cn = get_char()) == '=') {
+                put_token(TOKEN_OP_GE, 0);
+            } else {
+                unget_char();
+                put_token(TOKEN_OP_GT, 0);
+            }
         } else if (c == '{') {
+            put_token(TOKEN_SP_LCURBRACE, 0);
         } else if (c == '}') {
+            put_token(TOKEN_SP_RCURBRACE, 0);
         } else if (c == '[') {
+            put_token(TOKEN_SP_LSQRBRACKET, 0);
         } else if (c == ']') {
+            put_token(TOKEN_SP_RSQRBRACKET, 0);
         } else if (c == '(') {
+            put_token(TOKEN_SP_LRNDBRACKET, 0);
         } else if (c == ')') {
+            put_token(TOKEN_SP_RRNDBRACKET, 0);
         } else if (c == ';') {
+            put_token(TOKEN_SP_SEMICOLON, 0);
         } else if (c == ',') {
+            put_token(TOKEN_SP_COMMA, 0);
+        } else {
+            error();
         }
     }
     return 0;
